451db2e22be0ef16b0ada2f08977c09a84838535
Froax <31766004+Froax@users.noreply.github.com>
2017-11-15 10:16:30 +0100

    Update NS.py

--- NS.py
+++ NS.py
@@ -1,23 +1,53 @@
-import xmltodict
+def standaardprijs(afstandKM):
+    if afstandKM > 50:
+        return 15 + (afstandKM - 50) * 0.6
+    elif afstandKM <= 0:
+        return 0
+    else:
+        return afstandKM * 0.8
 
-def verwerk_xml():
-    bestand = open('stations.xml', 'r')
-    xml_string = bestand.read()
-    return xmltodict.parse(xml_string)
 
-stations = verwerk_xml()
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    standaard = standaardprijs(afstandKM)
+    prijs = 0
+    if leeftijd <= 12 or leeftijd >= 65:
+        if weekendrit:
+            prijs = standaard * 0.65
+        else:
+            prijs = standaard * 0.7
+    elif weekendrit:
+        prijs = standaard * 0.6
+    return prijs
 
-print('Dit zijn de codes en types van 4 stations: ')
-for station in stations['Stations']['Station']:
-    print('{:4} - {}'.format(station['Code'], station['Type']))
-
-print('\nDit zijn alle stations met een of meer synoniemen: ')
-
-for station in stations['Stations']['Station']:
-    if station['Synoniemen'] is not None:
-        for synoniem in station['Synoniemen']['Synoniem']:
-            print('{} - {}'.format(station['Code'], synoniem))
-
-print('Dit is de lange naam van elk station: ')
-for station in stations['Stations']['Station']:
-    print('{:4} - {}'.format(station['Code'], station['Namen']['Lang']))
+print(ritprijs(12, False, 10))
+print(ritprijs(12, False, 60))
+print(ritprijs(12, False, 100))
+print(ritprijs(12, False, -1))
+print(ritprijs(12, True, 10))
+print(ritprijs(12, True, 60))
+print(ritprijs(12, True, 100))
+print(ritprijs(12, True, -1))
+print(ritprijs(11, False, 10))
+print(ritprijs(11, False, 60))
+print(ritprijs(11, False, 100))
+print(ritprijs(11, False, -1))
+print(ritprijs(11, True, 10))
+print(ritprijs(11, True, 60))
+print(ritprijs(11, True, 100))
+print(ritprijs(11, True, -1))
+print(ritprijs(64, False, 10))
+print(ritprijs(64, False, 60))
+print(ritprijs(64, False, 100))
+print(ritprijs(64, False, -1))
+print(ritprijs(64, True, 10))
+print(ritprijs(64, True, 60))
+print(ritprijs(64, True, 100))
+print(ritprijs(64, True, -1))
+print(ritprijs(65, False, 10))
+print(ritprijs(65, False, 60))
+print(ritprijs(65, False, 100))
+print(ritprijs(65, False, -1))
+print(ritprijs(65, True, 10))
+print(ritprijs(65, True, 60))
+print(ritprijs(65, True, 100))
+print(ritprijs(65, True, -1))
